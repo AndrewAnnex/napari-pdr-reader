beb6be2a3e3f2ab4b3d8c7134718704a6bec8779
Andrew Annex <ama6fy@virginia.edu>
2022-07-06 16:12:23 -0700

    test fix

--- src/napari_pdr_reader/_tests/test_reader.py
+++ src/napari_pdr_reader/_tests/test_reader.py
@@ -1,16 +1,5 @@
-import tempfile
-
-import pdr
-import pdr.downloaders
-
 from napari_pdr_reader import napari_get_reader
-
-
-def get_test_data():
-    with tempfile.TemporaryDirectory() as dir:
-        url_img = "https://pds-imaging.jpl.nasa.gov/data/mars2020/mars2020_mastcamz_sci_calibrated/data/0061/rad/ZL0_0061_0672355882_040RAD_N0032046ZCAM05036_048050A03.IMG"
-        img_path = pdr.downloaders.download_data_and_label(url_img, data_dir=dir)
-        return img_path
+from napari_pdr_reader._sample_data import get_m2020_data
 
 
 # tmp_path is a pytest fixture
@@ -18,7 +7,7 @@ def test_reader(tmp_path):
     """An example of how you might test your plugin."""
 
     # write some fake data using your supported file format
-    my_test_file = get_test_data()
+    my_test_file = get_m2020_data()
 
     # try to read it back in
     reader = napari_get_reader(my_test_file)
